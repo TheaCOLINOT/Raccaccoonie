ba45407f91bd24799a243ed3113e56c51ac6296b
Théa <94928249+TheaCOLINOT@users.noreply.github.com>
2024-07-07 18:09:50 +0200

    debug

--- rdv.php
+++ rdv.php
@@ -27,6 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $html = <<<HTML
 <!DOCTYPE html>
 <html lang="fr">
+<link rel="stylesheet" href="style.css" />
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
